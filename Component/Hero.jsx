36bda697f0752dbc33e7ac391b34964b039653d9
indrawara017 <127364646+indrawara017@users.noreply.github.com>
2023-10-27 14:18:32 +0700

    Hero.jsx

--- Component/Hero.jsx
+++ Component/Hero.jsx
@@ -1,16 +1,8 @@
-import myImage from "../images/hero.jpg";
-import React from "react";
-import "../App.css";
+// function component
 export default function Hero() {
-  return (
-    <div>
-      <div className="card">
-        <img src={myImage} className="img-fluid" alt="..." />
-        <div className="card-img-overlay">
-          <h5 className="card-title">Selamat Datang</h5>
-          <p className="card-text">di website Praktikum Pemograman Website</p>
-        </div>
+    return (
+      <div>
+        <h1>INI HERO</h1>
       </div>
-    </div>
-  );
-}
+    );
+  }
